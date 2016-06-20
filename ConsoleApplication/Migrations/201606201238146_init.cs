98128d13e0bfc060831917c1ee9a1d90d8c40b2d
Teregulov <info@teregulov.com>
2016-06-20 15:43:50 +0300

    enable migrations and add init migration

---
+++ ConsoleApplication/Migrations/201606201238146_init.cs
@@ -0,0 +1,41 @@
+namespace ConsoleApplication.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Authors",
+                c => new
+                    {
+                        AuthorId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.AuthorId);
+            
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        BookId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        AuthorId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.BookId)
+                .ForeignKey("dbo.Authors", t => t.AuthorId, cascadeDelete: true)
+                .Index(t => t.AuthorId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Books", "AuthorId", "dbo.Authors");
+            DropIndex("dbo.Books", new[] { "AuthorId" });
+            DropTable("dbo.Books");
+            DropTable("dbo.Authors");
+        }
+    }
+}
